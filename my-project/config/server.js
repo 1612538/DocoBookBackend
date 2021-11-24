1f3ccc7ec48b4232e43cf0ddaaf1c6b852026483
1612538 <nhockid369@gmail.com>
2021-11-24 16:27:42 +0700

    First commit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec2ada6bae129a31c173acac6bbf64b2'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b242afd-423a-48ca-904e-5561cc5a6617'
+};
